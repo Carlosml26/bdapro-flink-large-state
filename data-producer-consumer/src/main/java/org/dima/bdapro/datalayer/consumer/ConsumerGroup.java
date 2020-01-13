81616be53ca5259059101e40a89370d1b7e167ac
carlosmartinez <martinezlorenzo@campus.tu-berlin.de>
2020-01-14 00:35:16 +0100

    First draft consumer

--- data-producer-consumer/src/main/java/org/dima/bdapro/datalayer/consumer/ConsumerGroup.java
+++ data-producer-consumer/src/main/java/org/dima/bdapro/datalayer/consumer/ConsumerGroup.java
@@ -1,4 +1,49 @@
 package org.dima.bdapro.datalayer.consumer;
 
-public class ConsumerGroup {
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ConsumerGroup {
+
+    private final int numberOfConsumers;
+    private final String groupId;
+    private final String topic;
+    private final String brokers;
+    private List<ConsumerThread> consumers;
+
+    public ConsumerGroup(String brokers, String groupId, String topic,
+                                     int numberOfConsumers) {
+        this.brokers = brokers;
+        this.topic = topic;
+        this.groupId = groupId;
+        this.numberOfConsumers = numberOfConsumers;
+        consumers = new ArrayList<>();
+        for (int i = 0; i < this.numberOfConsumers; i++) {
+            ConsumerThread ncThread =
+                    new ConsumerThread(this.brokers, this.groupId, this.topic);
+            consumers.add(ncThread);
+        }
+    }
+
+    public void execute() {
+        for (ConsumerThread ncThread : consumers) {
+            Thread t = new Thread(ncThread);
+            t.start();
+        }
+    }
+
+    /**
+     * @return the numberOfConsumers
+     */
+    public int getNumberOfConsumers() {
+        return numberOfConsumers;
+    }
+
+    /**
+     * @return the groupId
+     */
+    public String getGroupId() {
+        return groupId;
+    }
+
 }
