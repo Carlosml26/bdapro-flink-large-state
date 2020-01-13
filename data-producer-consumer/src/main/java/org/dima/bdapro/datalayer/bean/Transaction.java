2f428e00ae4ec4ddb1d4f3bf8f4b7f1a09b66f2f
Danish Amjad <danishamjad30@gmail.com>
2020-01-13 23:59:29 +0100

    Transaction VO

--- data-producer-consumer/src/main/java/org/dima/bdapro/datalayer/bean/Transaction.java
+++ data-producer-consumer/src/main/java/org/dima/bdapro/datalayer/bean/Transaction.java
@@ -1,4 +1,76 @@
 package org.dima.bdapro.datalayer.bean;
 
 public class Transaction {
+	Long transactionTime;
+	String transactionId;
+
+	String senderId;
+	String senderType;
+
+	String receiverId;
+	String receiverType;
+
+//	String profileId;
+
+	Double transactionAmount;
+//	Double transactionFee;
+//	Double transactionBonus;
+
+
+	public Long getTransactionTime() {
+		return transactionTime;
+	}
+
+	public void setTransactionTime(Long transactionTime) {
+		this.transactionTime = transactionTime;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public String getSenderId() {
+		return senderId;
+	}
+
+	public void setSenderId(String senderId) {
+		this.senderId = senderId;
+	}
+
+	public String getSenderType() {
+		return senderType;
+	}
+
+	public void setSenderType(String senderType) {
+		this.senderType = senderType;
+	}
+
+	public String getReceiverId() {
+		return receiverId;
+	}
+
+	public void setReceiverId(String receiverId) {
+		this.receiverId = receiverId;
+	}
+
+	public String getReceiverType() {
+		return receiverType;
+	}
+
+	public void setReceiverType(String receiverType) {
+		this.receiverType = receiverType;
+	}
+
+
+	public Double getTransactionAmount() {
+		return transactionAmount;
+	}
+
+	public void setTransactionAmount(Double transactionAmount) {
+		this.transactionAmount = transactionAmount;
+	}
 }
