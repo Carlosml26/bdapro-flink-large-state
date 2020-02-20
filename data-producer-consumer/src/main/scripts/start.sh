899f27997a3cee9429de54f1e9fb606a338479bd
cmcuza <cmcuza@gmail.com>
2020-02-20 14:44:28 +0100

    some local changes

--- data-producer-consumer/src/main/scripts/start.sh
+++ data-producer-consumer/src/main/scripts/start.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
+
+cd /home/cmcuza/IdeaProjects/bdapro-flink-large-state/data-producer-consumer/src/main/scripts
 export KAFKA_HOME="/usr/local/kafka/"
-=======
-cd /home/danish/FastWorkspace/BDMA/TUB/BDAPRO/Project/bdapro-flink-large-state/data-producer-consumer/src/main/scripts
->>>>>>> flink-consumer
 bash start-zookeeper.sh &
 sleep 5
 bash start-kafka.sh &
