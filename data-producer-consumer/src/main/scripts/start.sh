702f32308bdce2c82fda878183ebea2fa5dcd737
cmcuza <cmcuza@gmail.com>
2020-02-21 22:49:56 +0100

    no commit

--- data-producer-consumer/src/main/scripts/start.sh
+++ data-producer-consumer/src/main/scripts/start.sh
@@ -5,7 +5,7 @@ export KAFKA_HOME="/usr/local/kafka/"
 #change this to your own root dir
 export ROOT_DIR="/home/cmcuza/IdeaProjects/bdapro-flink-large-state/"
 
-cd data-producer-consumer/src/main/scripts
+#cd data-producer-consumer/src/main/scripts
 bash start-zookeeper.sh &
 sleep 5
 bash start-kafka.sh &
