0ef14feff5ea4f7f122e76a8cc4f106f1f9133bd
Danish Amjad <danishamjad30@gmail.com>
2020-02-24 21:03:49 +0100

    Fixed Transaction Deserializer Added Grafana Dashboard.

--- data-producer-consumer/src/main/java/org/dima/bdapro/datalayer/bean/bytesarray/TransactionDeserializer.java
+++ data-producer-consumer/src/main/java/org/dima/bdapro/datalayer/bean/bytesarray/TransactionDeserializer.java
@@ -4,7 +4,9 @@ import org.apache.commons.lang3.SerializationUtils;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.dima.bdapro.datalayer.bean.Transaction;
 
-public class TransactionDeserializer implements Deserializer<Transaction> {
+import java.io.Serializable;
+
+public class TransactionDeserializer implements Deserializer<Transaction>, Serializable {
 
 	@Override
 	public Transaction deserialize(String s, byte[] bytes) {
